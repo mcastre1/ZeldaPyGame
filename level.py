837dfb3b32c578af8eeeefbfaec55f887c7f99ed
mcastre1 <miguelcastrejongal@gmail.com>
2023-08-25 07:20:12 -0600

    Health and energy bar ui.

--- level.py
+++ level.py
@@ -6,6 +6,7 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 class Level:
     def __init__(self):
@@ -21,6 +22,9 @@ class Level:
 
         # sprite setup
         self.create_map()
+
+        # user interface
+        self.ui = UI()
 
     def create_map(self):
         layouts = {
@@ -65,6 +69,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
