6ab5bd966174189198b617d4f121bf7b40684253
mcastre1 <miguelcastrejongal@gmail.com>
2023-08-27 21:59:24 -0600

    Enemy spawned through tiled id.

--- level.py
+++ level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
     def __init__(self):
@@ -30,7 +31,8 @@ class Level:
         layouts = {
             'boundary': import_csv_layout("./map/map_FloorBlocks.csv"),
             'grass' : import_csv_layout("./map/map_Grass.csv"),
-            'object' : import_csv_layout("./map/map_Objects.csv")
+            'object' : import_csv_layout("./map/map_Objects.csv"),
+            'entities': import_csv_layout("./map/map_Entities.csv")
         }
 
         graphics = {
@@ -55,13 +57,20 @@ class Level:
                         if style == "object":
                             surf = graphics['objects'][int(col)]
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], "object", surf)
-        self.player = Player(
-            (2000, 1430),
-              [self.visible_sprites],
-                self.obstacle_sprites,
-                  self.create_attack,
-                    self.destroy_weapon,
-                    self.create_magic)
+
+                        if style == 'entities':
+                            if col == '394': # player
+                                self.player = Player(
+                                    (x, y),
+                                    [self.visible_sprites],
+                                        self.obstacle_sprites,
+                                        self.create_attack,
+                                            self.destroy_weapon,
+                                            self.create_magic)
+                                
+                            else:
+                                Enemy('monster', (x, y), [self.visible_sprites])
+                            
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites])
